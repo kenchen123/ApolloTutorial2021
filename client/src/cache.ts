db3970c6e85ce348d256357594b0dfeccada6b4b
Ken Chen <kenchen@equuscs.com>
2021-04-20 18:13:08 +0800

    login

--- client/src/cache.ts
+++ client/src/cache.ts
@@ -1,12 +1,22 @@
-import { InMemoryCache, Reference } from '@apollo/client';
+import { InMemoryCache, Reference, makeVar } from '@apollo/client';
 
 export const cache: InMemoryCache = new InMemoryCache({
     typePolicies: {
       Query: {
         fields: {
-          launches: {
-            keyArgs: false,
-            merge(existing, incoming) {
+            isLoggedIn: {
+                read() {
+                  return isLoggedInVar();
+                } 
+            },
+            cartItems: {
+                read() {
+                  return cartItemsVar();
+                } 
+            },
+            launches: {
+                keyArgs: false,
+                merge(existing, incoming) {
               let launches: Reference[] = [];
               if (existing && existing.launches) {
                 launches = launches.concat(existing.launches);
@@ -24,4 +34,10 @@ export const cache: InMemoryCache = new InMemoryCache({
       }
     }
   });
-  
+
+// Initializes to true if localStorage includes a 'token' key,
+// false otherwise
+export const isLoggedInVar = makeVar<boolean>(!!localStorage.getItem('token'));
+
+// Initializes to an empty array
+export const cartItemsVar = makeVar<string[]>([]);
